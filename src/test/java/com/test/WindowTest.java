6deaf0f8a36c2a32b9865a797186f5f2937abb78
unknown <Trevor.WhiteLocal@GSO05201605.orasi.com>
2018-11-01 12:19:45 -0400

    Yee Haw

---
+++ src/test/java/com/test/WindowTest.java
@@ -0,0 +1,28 @@
+package com.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class WindowTest {
+  @Test
+  public void f() {
+	  System.setProperty("webdriver.chrome.driver", 
+				"C:\\Users\\Trevor.WhiteLocal\\Desktop\\Selenium\\chromedriver.exe");
+	  WebDriver driver = new ChromeDriver();
+	  driver.get("https://the-internet.herokuapp.com/windows");
+	  
+	  driver.findElement(By.xpath("//*[contains(text(), 'Click Here')]")).click();
+	  System.out.println(driver.getWindowHandles());
+	  for(String windowHandle : driver.getWindowHandles()) {
+		  driver.switchTo().window(windowHandle);
+		  if (driver.getTitle().equals("New Window")){
+			  break;
+		  }
+	  }
+	  String result = driver.findElement(By.xpath("/html/body/div/h3")).getText();
+	  System.out.println(result);
+	  driver.quit();
+  }
+}
